14f9b708567f10ecd22ff95a7c71c043f2e00eb0
Pavel Holba <pavel.holba@vsb.cz>
2024-10-30 09:01:56 +0100

    this will probably push compiled code, but shouldn't

--- lesson1/HelloWorld.c
+++ lesson1/HelloWorld.c
@@ -6,5 +6,9 @@ int main() {
   printf("This is horizontal tab  with two spaces\n");
   // This is the comment
   printf("This is horizontal tab    with tab \n");
+  /* Or we can create a longer multiline comment
+  like that for example. This text can span several
+  lines, the only thing that matter is whether it
+  is enclosed between these -> */
   return 0;
 } 
