16da2bd3c6f6798bfc983b7b22833033c16b2368
Pavel Holba <pavel.holba@vsb.cz>
2024-10-30 09:00:34 +0100

    this will probably push compiled code, but shouldn't

---
+++ lesson2/variables.c
@@ -0,0 +1,11 @@
+
+int main() {
+    // Declare a variable
+    int myNum;
+
+    // Assign a value to the variable
+    myNum = 15;
+
+    // Declare variable and immidiately assign a value to it
+    int myNum2 = 15;
+}
