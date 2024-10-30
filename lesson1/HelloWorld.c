0cbf7f7fc87afb1cef17f34b773556daea7735ae
Pavel Holba <pavel.holba@vsb.cz>
2024-10-30 07:38:32 +0100

    Initial commit

---
+++ lesson1/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("This is horizontal tab\twith \\t\n");
+  printf("This is horizontal tab  with two spaces\n");
+  // This is the comment
+  printf("This is horizontal tab    with tab \n");
+  return 0;
+} 
